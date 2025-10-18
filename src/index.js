ead173e4ec591bbf6edbd214971516fa7a89dd95
deepak435-211 <silaparasettideepak1@gmail.com>
2025-10-18 19:27:00 +0530

    we

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Userdashboard from "./userdashboard/Dashboard";
+import RewardsDashboard from "./components/rewardsDashboard/rewardsDashboard";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -11,7 +12,7 @@ root.render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/userdashboard" element={<Userdashboard />} />
-
+        <Route path="/Rewards" element={<RewardsDashboard/>}/>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
